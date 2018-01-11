b56d9232ec5e71bffa676ec4daf191c3a9d79ad1
Ezio Savva <eziosavva@gmail.com>
2018-01-11 15:20:41 +0000

    6.02

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-(function () {
+(function (angular) {
 'use strict';
 
 angular.module('myFirstApp', [])
 
-.controller('MyFirstController', ['$scope' function($scope)  {
+.controller('MyFirstController', ['$scope' function($scope) {
   $scope.username = "Ezio";
 
-  $scope.sayHello = function () {
+  $scope.sayHello = function() {
     $scope.greeting = 'Hello ' + $scope.username + '!';
 
   };
-}]);
+}];
+})(window.angular);
