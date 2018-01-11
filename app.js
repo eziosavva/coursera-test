e5d4e1646c7d512e8f27945255c82a464469a4d9
Ezio Savva <eziosavva@gmail.com>
2018-01-11 15:50:50 +0000

    6.03

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 angular.module('myFirstApp', [])
 
-.controller('MyFirstController', ['$scope' function($scope) {
+.controller('MyFirstController', ['$scope', function($scope) {
   $scope.username = "Ezio";
 
   $scope.sayHello = function() {
